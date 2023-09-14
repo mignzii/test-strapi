110527c99e2561dafd40c3bb462eef2a9b240fba
Ibrahima Miniane Diouf <ibrahima.diouf@myriadgroup.com>
2023-09-14 15:01:41 +0000

    last commit

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,8 +1,8 @@
+
 module.exports = ({ env }) => ({
     proxy: true,
-    url: env('APP_URL'), // replaces `host` and `port` properties in the development environment
+    url: env('APP_URL'), // Sets the public URL of the application.
     app: { 
       keys: env.array('APP_KEYS')
     },
-  });
-  
+});
